425fbb0e16b12ef9eefcbcc038f586e9263540e7
uthamprabhu <uthamprabhu1@gmail.com>
2025-01-13 10:31:05 +0530

    sixth commit

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
-require('dotenv').config()
+
+const MONGO_URI = 'mongodb+srv://utham:12345@teslacardataset.zjq3c.mongodb.net/teslaDB?retryWrites=true&w=majority'
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URL);
+        await mongoose.connect(MONGO_URI);
         console.log('MongoDB connected successfully!');
     } catch (err) {
         console.error('MongoDB connection error:', err.message);
